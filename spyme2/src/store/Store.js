3a1f0b931dfe36baa0b587ccb8ad2267945b8b5e
iot-Noob <talhakhalid2018@gmail.com>
2025-09-26 12:32:35 +0500

    gitignore

--- spyme2/src/store/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import triggerReducer from "./TestSlice";
-
-export const store = configureStore({
-  reducer: {
-    trigger: triggerReducer,
-  },
-});
